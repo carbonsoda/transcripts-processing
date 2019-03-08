9608c8cce186d570cedc10e99277f454e8509b4c
Avery M <22334165+carbonsoda@users.noreply.github.com>
2019-03-08 13:01:47 -0500

    Update main.py

--- 1Sort/main.py
+++ 1Sort/main.py
@@ -1,3 +1,12 @@
-import pandas as pd
+""" 
+Node file for each subject and the respective data
 
-df = pd.read_excel
+The intention is for this to help sort/navigate to desired data
+Potentially useful to even then for exporting to different file types
+"""
+
+def __init__(self):
+    self._ID = None
+    self._NO = None
+    self._Age = None
+    self.transcript = None
