f3bd65b3d246598293bf7b4817c77ee63eb96d98
Avery M <22334165+carbonsoda@users.noreply.github.com>
2019-03-27 12:28:59 -0400

    Delete main.py

--- 1Sort/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def __init__(self):
-    # Needs to be set on initial run and converted into a raw string too, maybe? would I then concate w/ the name of subject or age?
-    self._path = None
-    self.script = None
